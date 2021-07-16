1aea289cde4ac84a06aa21270e667e3cf2697c7d
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-07-15 21:55:20 -0300

    Entity User e REST funcionando

--- src/main/java/com/joao/workshopmongo/WorkshopMongoApplication.java
+++ src/main/java/com/joao/workshopmongo/WorkshopMongoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopMongoApplication {
+public class WorkshopmongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopMongoApplication.class, args);
+		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
 
 }
--- src/test/java/com/joao/workshopmongo/WorkshopMongoApplicationTests.java
+++ src/test/java/com/joao/workshopmongo/WorkshopMongoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorkshopMongoApplicationTests {
+class WorkshopmongoApplicationTests {
 
 	@Test
 	void contextLoads() {
